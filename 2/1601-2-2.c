37280ab05a76d569cb02e1de30e67acdcbd07397
StellaVesperi <96127085+MinervaZhang@users.noreply.github.com>
2022-08-02 18:52:59 +0800

    Add files via upload

---
+++ 2/1601-2-2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()//我也不知道这个实验要干啥，反正代码是对的就好了。
+{int x,y;
+ printf("enter two numbers:\n");
+ scanf("%d%d",&x,&y);
+ if (x<=2&&x>=-2&&y<=2&&y>=-2)
+    printf("Yes\n");
+ else 
+    printf("No\n");
+ return 0;
+}
+
